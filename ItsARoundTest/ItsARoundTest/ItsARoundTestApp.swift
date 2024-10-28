17cc7b3ad3100b0d6e3fe86a4869721b3c1eb7f3
Aynur Nasybullin <nasybullin.aynur@yandex.ru>
2024-10-28 11:37:41 +0300

    created app;

---
+++ ItsARoundTest/ItsARoundTest/ItsARoundTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ItsARoundTestApp.swift
+//  ItsARoundTest
+//
+//  Created by Aynur Nasybullin on 28.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct ItsARoundTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
